146255f4044c0ec9ec56e39433dc92d33c889d07
kanghyeonseo <dlrj1234@naver.com>
2016-09-23 19:31:22 +0900

    A01.java실행

---
+++ A01.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class A01{
+      int age = 0;
+      public static void main(String[] args){
+             new A01().start();              
+
+     }
+     void start(){
+       Scanner s = new Scanner(System.in);
+       
+       System.out.print("나이를 입력 :");
+       int year = s.nextInt();
+       this.age = 2016 - year + 1;
+
+     }
+     
+     void print(){
+       if(age<20)
+       System.out.println("미성년자입니다.");
+       else
+       System.out.print("성인입니다.");
+     }
+
+}
+
+
