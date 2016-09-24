4a5f294bb73c39cab94282293b0d9332a1873a4d
김성찬 <dlrj1234@naver.com>
2016-09-24 19:18:32 +0900

    A02.java 실행완료

--- A02.java
+++ A02.java
@@ -1,27 +1,34 @@
 import java.util.*;
-class A01{
-      int age = 0;
-      public static void main(String[] args){
-             new A01().start();              
+    public class A02{
+        String kind = null;
+	double input_degree = 0;
+	public static void main(String[] args){
+	      new A02().start();
+	}
+        
+	  void start(){
+	    Scanner s = new Scanner(System.in);
 
-     }
-     void start(){
-       Scanner s = new Scanner(System.in);
-       
-       System.out.print("출생년도  입력 :");
-       int year = s.nextInt();
-       this.age = 2016 - year + 1;
-       print();
+	    System.out.print("온도를 입력하세요 :");
+	    this.input_degree = s.nextDouble();
+	    System.out.print("입력하신 온도가 섭씨온도이면 C를, 화씨온도이면 F를 입력하세요 :");
+	    this.kind = s.next();
+	    print();
+	 
+	}
 
-     }
-     
-     void print(){
-       if(age<20)
-       System.out.println("미성년자입니다.");
-       else
-       System.out.print("성인입니다.");
-     }
+	  void print(){
+             if(kind.equals("C")){
+                double output_degree = input_degree*1.8+32;
+		System.out.println("변환된 온도는" +output_degree+ "입니다");
+           }
+	     else{
+	        double output_degree = (input_degree-32)/1.8;
+		System.out.println("변환된 온도는" +output_degree+ "입니다");
+	   }
+	
+	}
 
-}
+ }
 
 
